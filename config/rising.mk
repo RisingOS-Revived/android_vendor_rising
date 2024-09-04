cd5bdbe9feb6f3c581f91aff3c3f6d32f7dd5d07
Tim Zimmermann <tim@linux4.de>
2024-09-04 21:26:56 +0200

    Rename hosts.real to etc_hosts
    * To avoid having to fork cuttlefish repo
    
    Change-Id: Ie38e74674a4918bad103a52dcb680aacb27db19a
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config/rising.mk
+++ config/rising.mk
@@ -16,11 +16,6 @@ endif
 -include vendor/google/mainline_modules/config.mk
 
 # Adblock
-PRODUCT_PACKAGES += \
-    hosts.adblock \
-    hosts.adblock_symlink \
-    hosts.real
-
 PRODUCT_COPY_FILES += \
     vendor/rising/etc/adblock/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
 
